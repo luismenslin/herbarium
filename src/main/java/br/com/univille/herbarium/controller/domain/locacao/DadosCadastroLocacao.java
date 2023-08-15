3d8aeb8bc920d142da96b63a081a54262e4c4cb1
Luis Menslin <luis.fmenslinx@gmail.com>
2023-08-15 07:08:58 -0300

    changes

--- src/main/java/br/com/univille/herbarium/controller/domain/locacao/DadosCadastroLocacao.java
+++
@@ -1,6 +0,0 @@
-package br.com.univille.herbarium.controller.domain.locacao;
-
-import java.time.LocalDate;
-
-public record DadosCadastroLocacao (Long idAluno, Long idLivro, LocalDate locacao) {
-}
--- src/main/java/br/com/univille/herbarium/controller/domain/locacao/LocacaoController.java
+++ src/main/java/br/com/univille/herbarium/controller/domain/locacao/LocacaoController.java
@@ -1,26 +1,15 @@
 package br.com.univille.herbarium.controller.domain.locacao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("locacao")
+@RequestMapping("locacoes")
 public class LocacaoController {
 
-    @Autowired
-    private LocacaoRepository repository;
     @GetMapping
-    public String showLocarLivro() {
-        return "/livros/locarLivro";
-    }
-
-    @PostMapping
-    public String locarLivro(DadosCadastroLocacao dados) {
-        var locacao = new Locacao(dados);
-        repository.save(locacao);
-        return "redirect:livros";
+    public String showLocacoes() {
+        return "/livros/listagemLocacoes";
     }
 }
