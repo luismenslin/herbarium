134f6379e2c921634734e686b31caaeddfb1ec3a
Fábio Menslin <luis.fmenslinx@gmail.com>
2023-08-18 20:06:13 -0300

    changes

--- src/main/java/br/com/univille/herbarium/controller/domain/livro/LivroController.java
+++ src/main/java/br/com/univille/herbarium/controller/domain/livro/LivroController.java
@@ -1,11 +1,14 @@
 package br.com.univille.herbarium.controller.domain.livro;
 
-import br.com.univille.herbarium.controller.domain.aluno.AlunoRepository;
+import br.com.univille.herbarium.controller.domain.locacao.LocacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @Controller
 @RequestMapping("livros")
@@ -13,6 +16,9 @@ public class LivroController {
 
     @Autowired
     private LivroRepository repository;
+
+    @Autowired
+    private LocacaoRepository locacaoRepository;
 
     @GetMapping
     public String showListagemLivros(Model model) {
@@ -54,7 +60,14 @@ public class LivroController {
     @DeleteMapping
     @Transactional
     public String removeLivro(Long id) {
+        var locacoes = locacaoRepository.findAll();
+        for (int i = 0;i < locacoes.size();i++) {
+            if (locacoes.get(i).getLivro().getId() == id) {
+                locacaoRepository.deleteById(locacoes.get(i).getId());
+            }
+        }
         repository.deleteById(id);
+
         return "redirect:livros";
     }
 }
